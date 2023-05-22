5239c94c128720ce8745c31efd9bd45708bb9c2e
up1 <somkiat.p@gmail.com>
2023-05-23 01:05:59 +0700

    Working with tracing and zipkin

--- hello/src/main/java/com/example/hello/HelloController.java
+++ hello/src/main/java/com/example/hello/HelloController.java
@@ -1,5 +1,8 @@
 package com.example.hello;
 
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    private ObservationRegistry observationRegistry;
+
     @GetMapping("hello/{name}")
     public HelloResponse sayHi(@PathVariable String name) {
+        Observation observation = Observation.createNotStarted("sayHi", this.observationRegistry);
+        observation.lowCardinalityKeyValue("some-tag", "some-value");
+        observation.observe(() -> {
+            // TODO
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
         return new HelloResponse(name);
     }
 
